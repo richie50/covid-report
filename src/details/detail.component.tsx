be81da4bb962933174ac76c07244d5fa97c6f368
Richie50 <7333599+richie50@users.noreply.github.com>
2020-11-28 17:43:32 -0500

    Initial commit

---
+++ src/details/detail.component.tsx
@@ -0,0 +1,94 @@
+import './details.css';
+
+import * as _ from 'lodash';
+
+import { Component } from "react";
+
+//const Detail = ({ match }) => <p>{match.params.id}</p>
+interface Props {
+  history: History;
+  location: Location;
+  match: any;
+}
+
+interface Details{
+    country: string,
+    lat:number,
+    lng:number,
+    confirmed: number,
+    deaths: number,
+    recovered: number,
+    active: number,
+    date: Date
+}
+interface State {
+  stats: Details[];
+}
+
+class CountryDetails extends Component<Props, State> {
+  state = { stats: [] };
+
+  componentDidMount() {
+    fetch(
+      `https://api.covid19api.com/live/country/${this.props.match.params.id}`
+    )
+      .then((res) => res.json())
+      .then((data) => {
+    
+        data =  _.map(data , function(mapped){
+            return {
+                country: mapped.Country,
+                lat: mapped.Lat,
+                lng: mapped.Lon,
+                confirmed: mapped.Confirmed,
+                deaths: mapped.Deaths,
+                recovered: mapped.Recovered,
+                active: mapped.Active,
+                date: mapped.Date
+
+            };
+        });
+        console.log(data);
+        this.setState({ stats: data });
+      });
+  }
+
+  render() {
+    const headers =
+      this.state.stats.length > 0
+        ? Object.keys(this.state.stats[0]).map((r) => r.toUpperCase())
+        : [];
+    console.log(headers);
+    return (
+      <table>
+          <caption>
+              <h2 className="table-header">Details of Countries with the highest number of COVID Cases</h2>
+            </caption>
+          <thead>
+          <tr>
+            {headers.map((header) => {
+                return <th key={header}>{header}</th>;
+            })}
+             </tr>
+          </thead>
+          <tbody>
+        {this.state.stats.length > 0
+          ? this.state.stats.map((stat, index) => {
+              return (
+                  
+                      <tr key={index}>
+                        {
+                        headers.map((h) => {return h.toLowerCase() === 'date' ? <td key={index + h.toLowerCase()}>{new Date(stat[h.toLowerCase()]).toDateString()}</td> : <td key={index +  h.toLowerCase()}>{stat[h.toLowerCase()]}</td>}
+                        )}
+                     </tr>
+                 
+              );
+            })
+          : null}
+           </tbody>
+      </table>
+    );
+  }
+}
+
+export default CountryDetails;
---
+++ src/home/home.component.tsx
@@ -0,0 +1,69 @@
+import './home.css';
+
+import * as _ from 'lodash';
+
+import { Button, Card, CardActions, CardContent, CardHeader, Typography } from '@material-ui/core';
+import React, { Component } from 'react';
+
+import { Link } from 'react-router-dom'
+
+interface Confirmed{
+    Country: string,
+    TotalConfirmed: number,
+    TotalDeaths: number,
+    TotalRecovered: number,
+    Slug: string
+
+
+}
+
+class  HomeComponent extends Component{
+   
+
+    state = {
+        totals: new Array<Confirmed>()
+    }
+
+    componentDidMount() {
+        const header = new Headers({"x-access-token":"5cf9dfd5-3449-485e-b5ae-70a60e997864" , "Content-Type":"application/json"});
+        fetch('https://api.covid19api.com/summary' , {method: 'GET' , headers: header} ).then(res => res.json()).then((data)=> {
+            this.setState({totals: data.Countries})
+        }).catch(console.log);
+    }
+    
+
+    render(){
+        return(
+            <div>
+                {this.highestDailyCases().map((e , idx) => {
+                    console.log(e)
+                    return(  
+                    
+                        <Card className="card" key={idx} variant="elevation">
+                            <CardHeader className="header" title={<Typography color="textPrimary" gutterBottom>{e.Country}</Typography>}> Testing
+                            </CardHeader>
+                            <CardContent className="container" id={e.Slug}>
+                                <Typography color="secondary">Total Confirmed: {e.TotalConfirmed}</Typography>
+                                <Typography color="secondary" >Total Death: {e.TotalDeaths}</Typography>
+                                <Typography color="secondary">Total Recovered: {e.TotalRecovered}</Typography>
+                            </CardContent>
+                            <CardActions>
+                                <Button variant="contained">
+                                    <Link to={`/${e.Slug}`}> Details </Link>
+                                </Button>    
+                            </CardActions>
+                        </Card>
+                        
+                    );
+                })}
+            </div>
+        );
+    }
+
+    public highestDailyCases(){
+        const highestCases = _.orderBy(this.state.totals, ['TotalConfirmed'], ['desc']).splice(0 , 9);
+        return highestCases;
+    }
+}
+
+export default HomeComponent;
